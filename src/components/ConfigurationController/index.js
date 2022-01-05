b9339ca2258ac227290062254419b613008054ed
tejeshreddy17 <tejeshreddy17@gmail.com>
2022-01-05 20:52:25 +0000

    first change

---
+++ src/components/ConfigurationController/index.js
@@ -0,0 +1,11 @@
+// Write your code here
+const ConfigurationController = () => (
+  <div>
+    <h1>LayOut</h1>
+    <input type="checkbox" />
+    <input type="checkbox" />
+    <input type="checkbox" />
+  </div>
+)
+
+export default ConfigurationController
---
+++ src/components/Layout/index.js
@@ -0,0 +1,8 @@
+// Write your code here
+const Layout = () => (
+  <div>
+    <h1>borugfi</h1>
+  </div>
+)
+
+export default Layout
